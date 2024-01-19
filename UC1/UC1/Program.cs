95100c1f2c8aa27a4de8ccee0e1568b33064a984
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-01-19 11:51:26 +0530

    [Ajay] Add: Use Case 1 Program

---
+++ UC1/UC1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int present = 1;
+            int absent = 0;
+            Random rand = new Random();
+            int random_number = rand.Next(2);
+            if(random_number == present) 
+            {
+                Console.WriteLine("Present");
+            }
+            else
+            {
+                Console.WriteLine("Absent");
+            }
+            Console.ReadLine();
+        }
+    }
+}
