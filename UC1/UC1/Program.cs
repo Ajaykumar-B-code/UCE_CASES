c7862547ddeb896338e51e6b050e42f2d0417c8d
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-01-19 12:00:36 +0530

    [Ajay] Add: Use Case 4 Program

--- UC1/UC1/Program.cs
+++ UC1/UC1/Program.cs
@@ -1,48 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UC3
+namespace UC4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int absent = 0;
-            int part_time = 1;
-            int present = 2;
-
-            Random random = new Random();
-            int random_number = random.Next(3);
-            string wage_per_hour;
-            string hour;
-            if (random_number == absent)
+            Random rnd = new Random();
+            int random_number = rnd.Next(3);
+            String wage_per_hour;
+            String hour;
+            switch(random_number) 
             {
-                Console.WriteLine("Employee is Absent");
-
-            }
-            else if (random_number == part_time)
-            {
-                Console.WriteLine("Employee is Part_time");
-                Console.Write("Enter the wage per hour : ");
-                wage_per_hour = Console.ReadLine();
-                Console.Write("Enter number of hour : ");
-                hour = Console.ReadLine();
-                
-                
-            }
-            else
-            {
-                Console.WriteLine("Employee is Full time");
-                Console.Write("Enter the wage per hour : ");
-                wage_per_hour = Console.ReadLine();
-                Console.Write("Enter number of hour : ");
-                hour = Console.ReadLine();
-                Console.WriteLine(Convert.ToInt32(wage_per_hour) * Convert.ToInt32(hour));
+                case 0:
+                    Console.WriteLine("Employee is Full Time");
+                    Console.Write("Enter the wage per hour : ");
+                    wage_per_hour = Console.ReadLine();
+                    Console.Write("Enter number of hour : ");
+                    hour = Console.ReadLine();
+                    Console.WriteLine(Convert.ToInt32(wage_per_hour)*Convert.ToInt32(hour));
+                    break;
+                case 1:
+                    Console.WriteLine("Employee is Part_Time");
+                    Console.Write("Enter the wage per hour : ");
+                    wage_per_hour = Console.ReadLine();
+                    Console.Write("Enter number of hour : ");
+                    hour = Console.ReadLine();
+                    Console.WriteLine(Convert.ToInt32(wage_per_hour) * Convert.ToInt32(hour));
+                    break;
+                case 2:
+                    Console.WriteLine("Employee is Absent");
+                    break;
             }
             Console.ReadLine();
+
         }
     }
 }
