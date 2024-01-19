97657bfba7164d9de69a35cde92c7588d66dd00d
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-01-19 12:03:06 +0530

    [Ajay] Add: Use Case 6 Program

--- UC1/UC1/Program.cs
+++ UC1/UC1/Program.cs
@@ -4,39 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UC5
+namespace UC6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();  
-            int random_number = rnd.Next(3);
-            int wage_per_hour, hour,days;
-            Console.Write("Enter the wage per hour : ");
-            wage_per_hour = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter the number of days: ");
-            days = Convert.ToInt32(Console.ReadLine());
-          
-            if(random_number == 0) 
+            Random rnd = new Random();
+            int days = 0, hours = 0;
+            while (days < 20 && hours < 100)
             {
-                Console.WriteLine("Employee is Full time");
-                Console.Write("Enter number of hour : ");
-                hour = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine(wage_per_hour * hour * days);
+                int val = rnd.Next(3);
+                hours += val * 4;
+                days++;
+                Console.WriteLine($"val : {val}  Hours : {hours}   Days : {days}");
             }
-            else if(random_number == 1) 
-            {
-                Console.WriteLine("Employee is part_time");
-                Console.Write("Enter number of hour : ");
-                hour = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine(wage_per_hour * hour * days);
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-            }
-            Console.ReadLine();
+            int wages = hours * 20;
+            Console.WriteLine($"Wages : {wages}");
+            Console.ReadKey();
         }
     }
 }
