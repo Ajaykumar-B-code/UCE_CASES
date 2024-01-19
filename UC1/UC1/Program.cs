a4052997d9b744f3de14ce3f6ffd2fcf9f120394
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-01-19 11:58:19 +0530

    [Ajay] Add: Use Case 3 Program

--- UC1/UC1/Program.cs
+++ UC1/UC1/Program.cs
@@ -4,16 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UC2
+namespace UC3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int wage_per_hour = 20;
-            int total_hour = 8;
-            int total_wage = wage_per_hour * total_hour;
-            Console.WriteLine(total_wage);
+            int absent = 0;
+            int part_time = 1;
+            int present = 2;
+
+            Random random = new Random();
+            int random_number = random.Next(3);
+            string wage_per_hour;
+            string hour;
+            if (random_number == absent)
+            {
+                Console.WriteLine("Employee is Absent");
+
+            }
+            else if (random_number == part_time)
+            {
+                Console.WriteLine("Employee is Part_time");
+                Console.Write("Enter the wage per hour : ");
+                wage_per_hour = Console.ReadLine();
+                Console.Write("Enter number of hour : ");
+                hour = Console.ReadLine();
+                
+                
+            }
+            else
+            {
+                Console.WriteLine("Employee is Full time");
+                Console.Write("Enter the wage per hour : ");
+                wage_per_hour = Console.ReadLine();
+                Console.Write("Enter number of hour : ");
+                hour = Console.ReadLine();
+                Console.WriteLine(Convert.ToInt32(wage_per_hour) * Convert.ToInt32(hour));
+            }
             Console.ReadLine();
         }
     }
