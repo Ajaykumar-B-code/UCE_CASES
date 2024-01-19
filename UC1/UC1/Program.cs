829b392768caa07eb3ae0b7763d8eae2d1d4f105
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-01-19 12:01:49 +0530

    [Ajay] Add: Use Case 5 Program

--- UC1/UC1/Program.cs
+++ UC1/UC1/Program.cs
@@ -1,44 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UC4
+namespace UC5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();
+            Random rnd = new Random();  
             int random_number = rnd.Next(3);
-            String wage_per_hour;
-            String hour;
-            switch(random_number) 
+            int wage_per_hour, hour,days;
+            Console.Write("Enter the wage per hour : ");
+            wage_per_hour = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the number of days: ");
+            days = Convert.ToInt32(Console.ReadLine());
+          
+            if(random_number == 0) 
             {
-                case 0:
-                    Console.WriteLine("Employee is Full Time");
-                    Console.Write("Enter the wage per hour : ");
-                    wage_per_hour = Console.ReadLine();
-                    Console.Write("Enter number of hour : ");
-                    hour = Console.ReadLine();
-                    Console.WriteLine(Convert.ToInt32(wage_per_hour)*Convert.ToInt32(hour));
-                    break;
-                case 1:
-                    Console.WriteLine("Employee is Part_Time");
-                    Console.Write("Enter the wage per hour : ");
-                    wage_per_hour = Console.ReadLine();
-                    Console.Write("Enter number of hour : ");
-                    hour = Console.ReadLine();
-                    Console.WriteLine(Convert.ToInt32(wage_per_hour) * Convert.ToInt32(hour));
-                    break;
-                case 2:
-                    Console.WriteLine("Employee is Absent");
-                    break;
+                Console.WriteLine("Employee is Full time");
+                Console.Write("Enter number of hour : ");
+                hour = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(wage_per_hour * hour * days);
+            }
+            else if(random_number == 1) 
+            {
+                Console.WriteLine("Employee is part_time");
+                Console.Write("Enter number of hour : ");
+                hour = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(wage_per_hour * hour * days);
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
             }
             Console.ReadLine();
-
         }
     }
 }
